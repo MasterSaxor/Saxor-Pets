c31003c12d182cab23175c0d8a04572fff79b0a0
MasterSaxor <70054062+MasterSaxor@users.noreply.github.com>
2023-09-21 10:01:03 +0000

    demonstrated while loop, down-counter

--- cat.py
+++ cat.py
@@ -1,5 +1,6 @@
-# Demonstrates multiple (identical) function calls
+# Demonstrates a while loop, counting down
 
-print("meow")
-print("meow")
-print("meow")
+i = 3
+while i != 0:
+    print("meow")
+    i = i - 1
